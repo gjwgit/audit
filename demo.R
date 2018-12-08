5161ea5c36ac46b24b356a170523ae855af62ed1
Graham Williams <git@togaware.com>
2018-12-08 21:40:27 +0800

    Fix typo.

--- demo.R
+++ demo.R
@@ -150,6 +150,6 @@ if (Sys.getenv("DISPLAY") != "")
 }
 
 cat("
-Close the graphic window using Ctrl-W.
+Close the graphic window using Ctrl-w.
 Press Enter to finish the demonstration: ")
 invisible(readChar("stdin", 1))
