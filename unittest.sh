c146a4e4516b2fb78f90cebf647acf7eed60bc0a
Graham Williams <graham.williams@togaware.com>
2018-12-21 13:16:32 +0800

    Wrap expect within a shell script for more flexibility and generality.

--- unittest.sh
+++ unittest.sh
@@ -1,17 +1,20 @@
-#!/usr/bin/expect -f
+#!/bin/bash
 
-spawn ml install audit
-expect "*audit*Y/n*"
+MODEL="audit"
+
+/usr/bin/expect <<EOD
+spawn ml install ${MODEL}
+expect "*${MODEL}*Y/n*"
 send "Y\n"
 expect eof
 
-spawn ml readme audit
+spawn ml readme ${MODEL}
 expect eof
 
-spawn ml configure audit
+spawn ml configure ${MODEL}
 expect eof
 
-spawn ml demo audit
+spawn ml demo ${MODEL}
 expect "Press Enter to continue on to a Confusion Matrix: "
 send "\n"
 expect "Press Enter to continue on to a Risk Chart: "
@@ -20,4 +23,8 @@ expect "Press Enter to finish the demonstration: "
 send "\n"
 expect eof
 
-system ls -lh ~/.mlhub/audit/audit_rpart_riskchart.pdf
+EOD
+
+ls -lh ~/.mlhub/${MODEL}/audit_rpart_riskchart.pdf
+
+
